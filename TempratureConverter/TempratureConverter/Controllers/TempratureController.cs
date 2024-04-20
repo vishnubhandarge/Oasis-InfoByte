7f2830b7886b78c94dae8138fce7f9d55011b333
vishnubhandarge <110363596+vishnubhandarge@users.noreply.github.com>
2024-04-20 14:02:04 +0530

    Add files via upload

---
+++ TempratureConverter/TempratureConverter/Controllers/TempratureController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempratureConverter.Models;
+
+namespace TempratureConverter.Controllers
+{
+    public class TempratureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new TempratreUnits());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Convert(TempratreUnits model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            // Conversion logic
+            if (model.unit1)
+            {
+                // Convert from Celsius to Fahrenheit
+                model.unit2ValueF = (model.unit1ValueF * 9 / 5) + 32;
+            }
+            else
+            {
+                // Convert from Fahrenheit to Celsius
+                model.unit2ValueF = (model.unit1ValueF - 32) * 5 / 9;
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ TempratureConverter/TempratureConverter/Models/TempratreUnits.cs
@@ -0,0 +1,10 @@
+﻿namespace TempratureConverter.Models
+{
+    public class TempratreUnits
+    {
+        public bool unit1 { get; set; }
+        public bool unit2 { get; set; }
+        public float unit1ValueF { get; set; }
+        public float unit2ValueF { get; set; }
+    }
+}
